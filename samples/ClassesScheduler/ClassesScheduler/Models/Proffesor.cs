3b60f9ad2d5df063e540dbcb7a35f033bcbce183
Stevan Kostoski <stevcooo@gmail.com>
2017-01-10 16:04:08 +0100

    carousel viewcell.

--- samples/ClassesScheduler/ClassesScheduler/Models/Proffesor.cs
+++ samples/ClassesScheduler/ClassesScheduler/Models/Proffesor.cs
@@ -7,5 +7,13 @@
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
     }
 }
