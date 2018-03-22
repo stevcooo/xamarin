93eb20498948d7d7e6b9b0c262c39d9bb5b35830
Stevan Kostoski <stevan@evernear.com>
2018-03-22 21:31:57 +0100

    grid view.

--- samples/CustomCamera/CustomRenderer/MainPageCS.cs
+++ samples/CustomCamera/CustomRenderer/MainPageCS.cs
@@ -6,18 +6,7 @@ namespace CustomRenderer
 	{
 		public MainPageCS ()
 		{
-			Title = "Main Page";
-			Padding = new Thickness (0, 20, 0, 0);
-			Content = new StackLayout { 
-				Children = {
-					new Label { Text = "Camera Preview:" },
-					new CameraPreview {
-						Camera = CameraOptions.Rear,
-						HorizontalOptions = LayoutOptions.FillAndExpand,
-						VerticalOptions = LayoutOptions.FillAndExpand
-					} 
-				}
-			};
+			
 		}
 	}
 }
